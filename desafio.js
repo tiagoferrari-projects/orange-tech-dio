89a0b667e4e79cccf86bd56f5e526ac7f502fc71
Tiago Ferrari <69052612+tiagoferrari-projects@users.noreply.github.com>
2022-10-22 10:04:44 -0300

    upload resolucao do desafio

--- desafio.js
+++ desafio.js
@@ -9,3 +9,11 @@ Você terá 3 variáveis, sendo elas:
  Imprima no console o valor que será gasto para realizar esta viagem.
  */
 
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const gastoViagem = precoCombustivel * litrosConsumidos;
+
+console.log('Gasto Viagem R$' + gastoViagem.toFixed(2));
