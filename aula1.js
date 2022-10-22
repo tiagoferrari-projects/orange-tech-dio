83c6b7cce790cc8e27417500aeb0dd7e4be68d04
Tiago Ferrari <69052612+tiagoferrari-projects@users.noreply.github.com>
2022-10-22 09:28:19 -0300

    primeiro commit

---
+++ aula1.js
@@ -0,0 +1,25 @@
+
+
+// const não pode mudar o valor da variável.
+//const pi = 3.14;
+
+/* let pode mudar o valor da variável 
+let variavel = 10;
+console.log(variavel);
+
+variavel = 20
+console.log(variavel);
+*/
+
+let variavelSoma = 10 + 10
+console.log(variavelSoma)
+
+let variavelSubtracao = 10 - 10
+console.log(variavelSubtracao)
+
+let variavelMultiplicacao = 10 * 10
+console.log(variavelMultiplicacao)
+
+let variavelDivisao = 10 / 10
+console.log(variavelDivisao)
+
