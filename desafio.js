2ba2d5b795f60a281dcfa26e3a23b71f614fab2a
Tiago Ferrari <69052612+tiagoferrari-projects@users.noreply.github.com>
2022-10-22 09:36:57 -0300

    adicionado arquivo desafio

---
+++ desafio.js
@@ -0,0 +1,11 @@
+/* 
+Faça um programa para calcular o valor de uma viagem.
+Você terá 3 variáveis, sendo elas: 
+
+ - preço do combustível;
+ - gasto médio de combustível do carro por km;
+ - distância em km da viagem;
+
+ Imprima no console o valor que será gasto para realizar esta viagem.
+ */
+
