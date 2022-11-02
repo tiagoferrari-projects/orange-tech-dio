63144e8e688ab59ffe36d04e5b79aff999f6e351
Tiago Ferrari <tiago-projects@outlook.com>
2022-11-02 12:17:40 -0300

    importacao de funcoes

---
+++ 07 - importacao/funcoes-auxiliares.js
@@ -0,0 +1,11 @@
+// importação: importante para organização
+// mocked: simulação
+function gets() {
+    return 10
+}
+
+function print(texto) {
+    console.log(texto)
+}
+
+module.exports = { gets, print }
